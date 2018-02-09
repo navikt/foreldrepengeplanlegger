17a924be144021536799d7533e0876f3840539ce
Frode Hansen <frode.hansen@bekk.no>
2018-02-09 10:33:34 +0100

    Fjerne mock utsettelse

--- src/app/redux/reducers/utsettelseReducer.ts
+++ src/app/redux/reducers/utsettelseReducer.ts
@@ -34,7 +34,7 @@ export const utsettelse2: Utsettelsesperiode = {
 };
 
 // mockUtsettelser.push(utsettelse1);
-mockUtsettelser.push(utsettelse2);
+// mockUtsettelser.push(utsettelse2);
 
 const defaultState: UtsettelseState = {
 	dialogErApen: false,

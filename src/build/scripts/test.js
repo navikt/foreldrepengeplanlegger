b9dfcad7da00e292a3a6858fdc2a1474be3a5b84
Frode Hansen <frode.hansen@bekk.no>
2018-02-13 13:57:54 +0100

    Kjøre --watch kun på eget test script

--- src/build/scripts/test.js
+++ src/build/scripts/test.js
@@ -18,7 +18,10 @@ const jest = require('jest');
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+// if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+// 	argv.push('--watch');
+// }
+if (argv.indexOf('--watch') >= 0 && argv.indexOf('--coverage') < 0) {
 	argv.push('--watch');
 }
 

5459db38e7ecf0a6b071f9e2ba7242be3893a2fb
Frode Hansen <frode.hansen@bekk.no>
2018-02-15 13:20:05 +0100

    Disclaimer

--- src/app/Planlegger.app.tsx
+++ src/app/Planlegger.app.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import Uttaksplan from './containers/Uttaksplan';
 import './styles/planlegger.less';
+import Alertstripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
 
 export class PlanleggerIndex extends React.Component {
 	render() {
 		return (
 			<div className="planlegger">
+				<br />
+				<Alertstripe type="info">
+					<p>
+						Du har nå funnet en foreldrepengeplanlegger som er under utvikling
+						her i NAV.
+					</p>{' '}
+					<p>
+						<strong>Dette er ikke et søknadsskjema</strong>. Gå til{' '}
+						<Lenke href="https://nav.no/foreldrepenger">
+							nav.no/foreldrepenger
+						</Lenke>{' '}
+						for mer informasjon om foreldrepenger.
+					</p>
+				</Alertstripe>
 				<h1 className="m-textCenter">Foreldrepengeplanleggeren</h1>
 				<Uttaksplan />
 			</div>

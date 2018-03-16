c47919ad51140fa176cb40d1294e95eb492faac8
Frode Hansen <frode.hansen@bekk.no>
2018-03-16 13:51:57 +0100

    Fjerne log fra tester

--- src/app/utils/__tests/helligdagerUtils.test.ts
+++ src/app/utils/__tests/helligdagerUtils.test.ts
@@ -18,7 +18,6 @@ describe('fridagerUtils', () => {
 			startdato: new Date(2018, 0, 1),
 			sluttdato: new Date(2018, 11, 31)
 		});
-		console.log(fridager.map((f) => `${f.name}-${f.date} (${f.type}`));
 		expect(fridager.length).toBe(12);
 	});
 	it('henter ut alle fridager i 2018-2021', () => {
@@ -27,10 +26,5 @@ describe('fridagerUtils', () => {
 			sluttdato: new Date(2021, 11, 31)
 		});
 		expect(fridager.length).toBe(48);
-	});
-
-	it('henter ut fridager i en mai 2018', () => {
-		const fridager = getOffentligeFridagerIMåned(new Date(2018, 4, 1));
-		console.log(fridager);
 	});
 });

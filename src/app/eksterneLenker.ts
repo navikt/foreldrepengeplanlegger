b11108d4fb3ad3740500d895dc20de300d5a6933
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2018-10-26 12:50:12 +0200

    Legg inn en tilbakeknapp til familie.nav.no

--- src/app/eksterneLenker.ts
+++ src/app/eksterneLenker.ts
@@ -1,5 +1,6 @@
 const EksterneLenker = {
-	nav_foreldrepenger: 'https://nav.no/foreldrepenger',
+	nav_foreldrepenger: 'https://familie.nav.no/',
+	nav_foreldrepenger_les_mer: 'https://familie.nav.no/om-foreldrepenger',
 	nav_ulonnetPermisjon:
 		'https://www.nav.no/no/Person/Arbeid/Sykmeldt%2C+arbeidsavklaringspenger+og+yrkesskade/Sykepenger/Sykepenger+til+sarskilte+grupper',
 	nav_aktivitetskrav:

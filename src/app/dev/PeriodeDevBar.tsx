373eb0b697f7b041f404347e4608bb115ab39014
Frode Hansen <frode.hansen@bekk.no>
2019-01-15 12:07:07 +0100

    DevBar update

--- src/app/dev/PeriodeDevBar.tsx
+++ src/app/dev/PeriodeDevBar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Panel from 'nav-frontend-paneler';
-import { Periode, Uttaksperiode, Periodetype, Forelder } from '../types';
+import { Periode, Periodetype, Forelder, UtsettelsesårsakType } from '../types';
 import Knapperad from 'common/components/knapperad/Knapperad';
 import { Flatknapp } from 'nav-frontend-knapper';
 import { Uttaksdagen } from '../utils/Uttaksdagen';
@@ -24,22 +24,44 @@ const getNesteUttaksdag = (perioder: Periode[]) => {
 };
 
 class PeriodeDevBar extends React.Component<Props, {}> {
-    addAfter(forelder: Forelder, uttaksdager: number = 10) {
-        const periode: Uttaksperiode = {
-            id: guid(),
-            type: Periodetype.Uttak,
-            fixed: false,
-            forelder,
-            tidsperiode: getTidsperiode(getNesteUttaksdag(this.props.perioder), uttaksdager)
-        };
-        this.props.onAdd(periode);
+    addAfter(forelder: Forelder, periodetype: Periodetype, varighet: number = 10) {
+        const id = guid();
+        const tidsperiode = getTidsperiode(getNesteUttaksdag(this.props.perioder), varighet);
+        if (periodetype === Periodetype.Uttak) {
+            this.props.onAdd({
+                id,
+                forelder,
+                tidsperiode,
+                type: Periodetype.Uttak,
+                fixed: false
+            });
+        } else if (periodetype === Periodetype.Utsettelse) {
+            this.props.onAdd({
+                id,
+                forelder,
+                tidsperiode,
+                type: Periodetype.Utsettelse,
+                fixed: true,
+                årsak: UtsettelsesårsakType.Ferie
+            });
+        }
     }
     render() {
         return (
             <Panel border={true}>
                 <Knapperad>
-                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder1, 20)}>F1</Flatknapp>
-                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder2, 20)}>F2</Flatknapp>
+                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder1, Periodetype.Uttak, 20)}>
+                        Uttak F1
+                    </Flatknapp>
+                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder2, Periodetype.Uttak, 20)}>
+                        Uttak F2
+                    </Flatknapp>
+                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder1, Periodetype.Utsettelse, 5)}>
+                        Ferie F1
+                    </Flatknapp>
+                    <Flatknapp onClick={() => this.addAfter(Forelder.forelder2, Periodetype.Utsettelse, 5)}>
+                        Ferie F2
+                    </Flatknapp>
                 </Knapperad>
             </Panel>
         );

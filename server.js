40e282e17f71665d95a94937907ed2b1bd8ba347
Frode Hansen <frode.hansen@bekk.no>
2019-01-21 11:39:45 +0100

    Updater server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mustacheExpress = require('mustache-express');
 const Promise = require('promise');
 const getDecorator = require('./src/build/scripts/decorator');
 const compression = require('compression');
+const createEnvSettingsFile = require('./src/build/scripts/envSettings');
 
 const server = express();
 server.use(compression());
@@ -17,63 +18,44 @@ server.engine('html', mustacheExpress());
 createEnvSettingsFile(path.resolve(`${__dirname}/dist/js/settings.js`));
 
 server.use((req, res, next) => {
-	res.removeHeader('X-Powered-By');
-	next();
+    res.removeHeader('X-Powered-By');
+    next();
 });
 
-
 const renderApp = (decoratorFragments) =>
-	new Promise((resolve, reject) => {
-		server.render('index.html', decoratorFragments, (err, html) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(html);
-				}
-			}
-		);
-	});
+    new Promise((resolve, reject) => {
+        server.render('index.html', decoratorFragments, (err, html) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(html);
+            }
+        });
+    });
 
 const startServer = (html) => {
-	server.use(
-		'/foreldrepengeplanlegger/dist/js',
-		express.static(path.resolve(__dirname, 'dist/js'))
-	);
-	server.use(
-		'/foreldrepengeplanlegger/dist/css',
-		express.static(path.resolve(__dirname, 'dist/css'))
-	);
+    server.use('/foreldrepengeplanlegger/dist/js', express.static(path.resolve(__dirname, 'dist/js')));
+    server.use('/foreldrepengeplanlegger/dist/css', express.static(path.resolve(__dirname, 'dist/css')));
 
-	server.get(
-		[
-			'/',
-			'/foreldrepengeplanlegger/?',
-			/^\/foreldrepengeplanlegger\/(?!.*dist).*$/
-		],
-		(req, res) => {
-			res.send(html);
-		}
-	);
+    server.get(['/', '/foreldrepengeplanlegger/?', /^\/foreldrepengeplanlegger\/(?!.*dist).*$/], (req, res) => {
+        res.send(html);
+    });
 
-	server.get('/foreldrepengeplanlegger/internal/isAlive', (req, res) =>
-		res.sendStatus(200)
-	);
-	server.get('/foreldrepengeplanlegger/internal/isReady', (req, res) =>
-		res.sendStatus(200)
-	);
+    server.get('/foreldrepengeplanlegger/internal/isAlive', (req, res) => res.sendStatus(200));
+    server.get('/foreldrepengeplanlegger/internal/isReady', (req, res) => res.sendStatus(200));
 
-	server.get(/^\/(?!.*dist).*$/, (req, res) => {
-		res.send(html);
-	});
+    server.get(/^\/(?!.*dist).*$/, (req, res) => {
+        res.send(html);
+    });
 
-	const port = process.env.PORT || 8080;
-	server.listen(port, () => {
-		console.log(`App listening on port: ${port}`);
-	});
+    const port = process.env.PORT || 8080;
+    server.listen(port, () => {
+        console.log(`App listening on port: ${port}`);
+    });
 };
 
 const logError = (errorMessage, details) => console.log(errorMessage, details);
 
 getDecorator()
-	.then(renderApp, (error) => logError('Failed to get decorator', error))
-	.then(startServer, (error) => logError('Failed to render app', error));
+    .then(renderApp, (error) => logError('Failed to get decorator', error))
+    .then(startServer, (error) => logError('Failed to render app', error));

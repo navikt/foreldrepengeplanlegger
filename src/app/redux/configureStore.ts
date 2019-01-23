e93376c6ee0ea88f6b3e23d6a8094a0b712b0a1c
Frode Hansen <frode.hansen@bekk.no>
2019-01-23 12:59:34 +0100

    Change redux compose

--- src/app/redux/configureStore.ts
+++ src/app/redux/configureStore.ts
@@ -1,13 +1,13 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer, { AppState } from './reducers/rootReducer';
 import createSagaMiddleware from '@redux-saga/core';
 import rootSaga from './saga/rootSaga';
 
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 function configureStore(initialState?: Partial<AppState>) {
     const sagaMiddleware = createSagaMiddleware();
-    const composeEnhancer: typeof compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-    const store = createStore(rootReducer, initialState, composeEnhancer(applyMiddleware(sagaMiddleware)));
+    const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
     sagaMiddleware.run(rootSaga);
 

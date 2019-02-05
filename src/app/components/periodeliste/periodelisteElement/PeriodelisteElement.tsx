ac7e8e4c59f625bfbdc2035593d2eb229e73a8a5
Frode Hansen <frode.hansen@bekk.no>
2019-02-05 23:54:35 +0100

    Info om helligdag på en ferie

--- src/app/components/periodeliste/periodelisteElement/PeriodelisteElement.tsx
+++ src/app/components/periodeliste/periodelisteElement/PeriodelisteElement.tsx
@@ -2,15 +2,25 @@ import * as React from 'react';
 import classNames from 'classnames';
 import BEMHelper from 'common/utils/bem';
 import SlettKnapp from 'common/components/slett-knapp/SlettKnapp';
+import Sirkelknapp from 'common/components/sirkelknapp/Sirkelknapp';
+import InfoIkon from 'common/components/ikoner/InfoIkon';
+import Block from 'common/components/block/Block';
 
 import './periodelisteElement.less';
+import LukkInfoIkon from 'common/components/ikoner/LukkInfoIkon';
+import { guid } from 'nav-frontend-js-utils';
 
-interface OwnProps {
+interface Props {
     menyer: PeriodeElementMeny[];
     slett?: {
         onRemove: () => void;
         ariaLabel: string;
     };
+    info?: string[];
+}
+
+interface State {
+    infoVisible?: boolean;
 }
 
 interface PeriodeElementMeny {
@@ -29,24 +39,62 @@ const PeriodeElementMenyWrapper: React.StatelessComponent<{ meny: PeriodeElement
     );
 };
 
-class PeriodelisteElement extends React.Component<OwnProps, {}> {
+class PeriodelisteElement extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            infoVisible: false
+        };
+    }
     render() {
-        const { menyer, slett } = this.props;
+        const { menyer, slett, info } = this.props;
+        const infoId = guid();
         return (
             <div className={bem.block}>
-                {menyer
-                    .filter((meny) => (meny.isVisibleCheck ? meny.isVisibleCheck() : true))
-                    .map((meny) => (
-                        <PeriodeElementMenyWrapper meny={meny} key={meny.id} />
-                    ))}
+                <div className={bem.element('mainContent')}>
+                    {menyer
+                        .filter((meny) => (meny.isVisibleCheck ? meny.isVisibleCheck() : true))
+                        .map((meny) => (
+                            <PeriodeElementMenyWrapper meny={meny} key={meny.id} />
+                        ))}
 
-                {slett && (
-                    <div className={bem.element('tools')}>
-                        <SlettKnapp
-                            onClick={() => slett.onRemove()}
-                            ariaLabel={slett.ariaLabel}
-                            title={slett.ariaLabel}
-                        />
+                    {(slett || info) && (
+                        <div className={bem.element('tools')}>
+                            {slett && (
+                                <div className={bem.element('tool')}>
+                                    <SlettKnapp
+                                        onClick={() => slett.onRemove()}
+                                        ariaLabel={slett.ariaLabel}
+                                        title={slett.ariaLabel}
+                                    />
+                                </div>
+                            )}
+                            {info && (
+                                <div className={bem.element('tool')}>
+                                    {
+                                        <Sirkelknapp
+                                            ikon={this.state.infoVisible ? <LukkInfoIkon /> : <InfoIkon />}
+                                            stil="info"
+                                            toggle={{ pressed: this.state.infoVisible || false }}
+                                            ariaLabel="f"
+                                            onClick={() => this.setState({ infoVisible: !this.state.infoVisible })}
+                                            aria-describedby={infoId}
+                                        />
+                                    }
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
+                {info && (
+                    <div className={bem.element('info')} id={infoId}>
+                        <Block visible={this.state.infoVisible} animated={true} style="info" margin="none">
+                            {info.map((i, idx) => (
+                                <p key={idx} className={bem.element('infoText')}>
+                                    {i}
+                                </p>
+                            ))}
+                        </Block>
                     </div>
                 )}
             </div>

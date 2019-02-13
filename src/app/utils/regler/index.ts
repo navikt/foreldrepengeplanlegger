d33c0b75dcd6d6e30ade03922ab7be055b58edac
Frode Hansen <frode.hansen@bekk.no>
2019-02-13 10:39:55 +0100

    erInnenforFørsteOgSisteMuligeUttaksdag

--- src/app/utils/regler/index.ts
+++ src/app/utils/regler/index.ts
@@ -1,11 +1,16 @@
 import starterInnenfor12UkerFørTermin from './tester/starterInnenfor12UkerFørTermin';
 import { Regel } from './types';
 import { RegelKey } from './regelKeys';
+import erInnenforFørsteOgSisteMuligeUttaksdag from './tester/erInnenforF\u00F8rsteOgSisteMuligeUttaksdag';
 
 const uttaksplanRegler: Regel[] = [
     {
         key: RegelKey.starterInnenfor12UkerFørTermin,
         test: starterInnenfor12UkerFørTermin
+    },
+    {
+        key: RegelKey.erInnenforFørsteOgSisteMuligeUttaksdag,
+        test: erInnenforFørsteOgSisteMuligeUttaksdag
     }
 ];
 

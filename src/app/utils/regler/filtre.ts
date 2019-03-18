e36ec6b0ae65704ad9702cfac1a05ae5546f1ec5
Frode Hansen <frode.hansen@bekk.no>
2019-03-18 12:54:38 +0100

    Slett ubrukt kode

--- src/app/utils/regler/filtre.ts
+++
@@ -1,7 +0,0 @@
-import moment from 'moment';
-import { getUttaksdatoer } from '../uttaksdatoer';
-
-export const erInnenforSeksFørsteUker = (dato: Date, familiehendelsesdato: Date): boolean => {
-    const { sisteUttaksdagInnenforSeksUker } = getUttaksdatoer(familiehendelsesdato).etterFødsel;
-    return moment(dato).isSameOrBefore(sisteUttaksdagInnenforSeksUker, 'day');
-};

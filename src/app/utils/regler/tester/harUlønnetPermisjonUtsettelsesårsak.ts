4ed20b9694378954d59e8bf287d2b6502ad26c7a
Frode Hansen <frode.hansen@bekk.no>
2019-04-05 08:55:46 +0200

    Ikke kjøre regel på ulønnet permisjon i slutten av plan

--- src/app/utils/regler/tester/harUlønnetPermisjonUtsettelsesårsak.ts
+++ src/app/utils/regler/tester/harUlønnetPermisjonUtsettelsesårsak.ts
@@ -5,23 +5,30 @@ import {
     RegelTestresultatInfoObject,
     RegelTestresultatInfo
 } from '../types';
-import { isUlønnetPermisjon } from '../../../types';
+
+import { isUlønnetPermisjon, Forelder, Periodetype } from '../../../types';
 
 export const harUlønnetPermisjonUtsettelsesårsak: RegelTest = (grunnlag: Regelgrunnlag): RegelTestresultat => {
-    const perioderUtenÅrsak = grunnlag.perioder.filter(
-        (p) => isUlønnetPermisjon(p) && p.utsettelsesårsak === undefined
-    );
+    // Regel gjelder ikke ulønnede permisjoner på slutten av uttaksplanen
+    const idx = grunnlag.perioder
+        .slice()
+        .reverse()
+        .findIndex((p) => p.type !== Periodetype.UlønnetPermisjon);
+
+    const perioder = idx > -1 ? grunnlag.perioder.slice(0, grunnlag.perioder.length - idx) : grunnlag.perioder;
+    const perioderUtenÅrsak = perioder.filter((p) => isUlønnetPermisjon(p) && p.utsettelsesårsak === undefined);
     const antallPerioderUtenÅrsak = perioderUtenÅrsak.length;
-    let passerer = antallPerioderUtenÅrsak === 0;
-    if (antallPerioderUtenÅrsak === 1) {
-        const sistePeriode = perioderUtenÅrsak[antallPerioderUtenÅrsak - 1];
-        if (sistePeriode.id === grunnlag.perioder[grunnlag.perioder.length - 1].id) {
-            passerer = true;
-        }
-    }
+
+    const passerer = antallPerioderUtenÅrsak === 0;
 
     const info: RegelTestresultatInfoObject = perioderUtenÅrsak.map(
-        (p): Partial<RegelTestresultatInfo> => ({ periodeId: p.id })
+        (p): Partial<RegelTestresultatInfo> => ({
+            periodeId: p.id,
+            values: {
+                forelderNavn: p.forelder === Forelder.mor ? grunnlag.navnMor : grunnlag.navnFarMedmor,
+                utsetterNavn: p.forelder === Forelder.mor ? grunnlag.navnFarMedmor : grunnlag.navnMor
+            }
+        })
     );
     return {
         passerer,

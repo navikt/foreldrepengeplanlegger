9ffc9b104e95b4b7f52fdd0df8cafe70331547c7
Frode Hansen <frode.hansen@bekk.no>
2019-04-05 10:27:51 +0200

    Fjern utsettelsesårsak på avsluttende ulønnede permisjoner

--- src/app/utils/Builder.ts
+++ src/app/utils/Builder.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { Periode } from '../types/periodetyper';
+import { Periode, isUlønnetPermisjon } from '../types/periodetyper';
 import { Periodene, sorterPerioder } from './Periodene';
 import { Uttaksdagen } from './Uttaksdagen';
 import { Perioden } from './Perioden';
@@ -40,8 +40,10 @@ class Builder {
             this.perioder = settInnPerioder(this.perioder, fastePerioder);
             this.sort();
             this.perioder = slåSammenLikePerioder(this.perioder);
+            this.perioder = fjernUtsettelsesårsakPåAvsluttendeUlønnedePermisjoner(this.perioder);
         } else {
             this.perioder = resetTidsperioder(this.perioder, this.familiehendelsesdato);
+            this.perioder = fjernUtsettelsesårsakPåAvsluttendeUlønnedePermisjoner(this.perioder);
         }
 
         return this;
@@ -260,4 +262,23 @@ function splittPeriodeMedPeriode(periode: Periode, nyPeriode: Periode): Periode[
         tidsperiode: getTidsperiode(startSisteDel, dagerSisteDel)
     };
     return [forste, midt, siste];
+}
+
+function fjernUtsettelsesårsakPåAvsluttendeUlønnedePermisjoner(perioder: Periode[]): Periode[] {
+    let annenTypePeriodeFunnet = false;
+    const revPerioder = perioder
+        .slice()
+        .reverse()
+        .map((periode) => {
+            if (isUlønnetPermisjon(periode) && annenTypePeriodeFunnet === false) {
+                return {
+                    ...periode,
+                    utsettelsesårsak: undefined
+                };
+            }
+            annenTypePeriodeFunnet = true;
+            return periode;
+        })
+        .reverse();
+    return revPerioder;
 }

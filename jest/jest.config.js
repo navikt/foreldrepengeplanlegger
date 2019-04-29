96427ab6c98f7a9abd1051c13a09c13c14c939fe
Frode Hansen <frode.hansen@bekk.no>
2019-04-29 18:25:29 +0200

    Slette gammel jest config

--- jest/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'js-with-babel',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '\\.(css|jpg|png|svg|less)$': '<rootDir>/node_modules/jest-css-modules',
-  },
-  rootDir: './src'
-};

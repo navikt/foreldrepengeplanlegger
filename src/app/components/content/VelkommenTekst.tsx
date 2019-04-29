1d14a0e9ff8fb1c6fada64d72f13798e7054ec0d
Frode Hansen <frode.hansen@bekk.no>
2019-04-29 13:21:08 +0200

    Skjul tekst

--- src/app/components/content/VelkommenTekst.tsx
+++ src/app/components/content/VelkommenTekst.tsx
@@ -9,29 +9,31 @@ import getMessage from 'common/utils/i18nUtils';
 import { injectIntl, InjectedIntlProps, FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 import UtvidetInformasjon from 'common/components/utvidetInformasjon/UtvidetInformasjon';
 
-const VelkommenTekst: React.StatelessComponent<RouteComponentProps & InjectedIntlProps> = ({ intl, location }) => (
-    <Introduksjon
-        ikon={<IntroSirkelSvg />}
-        tittel={getMessage(intl, 'intro.tittel')}
-        skjulContent={location.pathname !== AppRoutes.startside}>
-        <Block margin="xxs">
-            <Block margin="s">
-                <Ingress tag="div">
-                    <FormattedMessage id="intro.tekst" />
-                </Ingress>
-            </Block>
-            <UtvidetInformasjon apneLabel={getMessage(intl, 'intro.begrensninger.tittel')}>
+const VelkommenTekst: React.StatelessComponent<RouteComponentProps & InjectedIntlProps> = ({ intl, location }) => {
+    return (
+        <Introduksjon
+            ikon={<IntroSirkelSvg />}
+            tittel={getMessage(intl, 'intro.tittel')}
+            skjulContent={location.pathname !== AppRoutes.startside && location.pathname !== `${AppRoutes.startside}/`}>
+            <Block margin="xxs">
                 <Block margin="s">
-                    <Block margin="xs">
-                        <Element tag="h3">
-                            <FormattedMessage id="intro.begrensninger.tittel" />
-                        </Element>
-                    </Block>
-                    <FormattedHTMLMessage id="intro.begrensninger.html" tagName="div" />
+                    <Ingress tag="div">
+                        <FormattedMessage id="intro.tekst" />
+                    </Ingress>
                 </Block>
-            </UtvidetInformasjon>
-        </Block>
-    </Introduksjon>
-);
+                <UtvidetInformasjon apneLabel={getMessage(intl, 'intro.begrensninger.tittel')}>
+                    <Block margin="s">
+                        <Block margin="xs">
+                            <Element tag="h3">
+                                <FormattedMessage id="intro.begrensninger.tittel" />
+                            </Element>
+                        </Block>
+                        <FormattedHTMLMessage id="intro.begrensninger.html" tagName="div" />
+                    </Block>
+                </UtvidetInformasjon>
+            </Block>
+        </Introduksjon>
+    );
+};
 
 export default withRouter(injectIntl(VelkommenTekst));
